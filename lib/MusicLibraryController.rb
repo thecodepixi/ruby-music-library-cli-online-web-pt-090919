22b0ae4c4a59252ac1e9888362175ea1dcb67db4
Emily Harber <emily.a.harber@gmail.com>
2019-10-19 23:14:43 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -19,6 +19,9 @@ class MusicLibraryController
     until user_input == "exit"
       puts "What would you like to do?"
       user_input = gets.chomp 
+      if user_input == "list songs"
+        list_songs 
+      end 
     end 
    # this ends the call method  
   end 
