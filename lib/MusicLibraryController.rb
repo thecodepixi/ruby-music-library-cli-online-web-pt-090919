6acd77dc728002a84338fe929711647fc6431f2e
Emily Harber <emily.a.harber@gmail.com>
2019-10-19 22:29:29 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -26,7 +26,8 @@ class MusicLibraryController
   def list_songs 
     sorted_songs = Song.all.sort { |song_a, song_b| song_a.name <=> song_b.name }
     sorted_songs.each do |song| 
-      puts "#{song.index + 1}. #{song.artist.name} - #{song.name} - #{song.genre}"
+      index = 0 
+      puts "#{index += 1}. #{song.artist.name} - #{song.name} - #{song.genre}"
     end 
   end 
  #this ends the class  
