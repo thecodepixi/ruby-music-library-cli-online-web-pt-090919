c06ccc89de79b353a8d5fa9fbe76b8ba2297d502
Emily Harber <emily.a.harber@gmail.com>
2019-10-19 23:16:28 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -28,6 +28,8 @@ class MusicLibraryController
           list_genres
         when "list artist"
           list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
         end 
     end 
    # this ends the call method  
