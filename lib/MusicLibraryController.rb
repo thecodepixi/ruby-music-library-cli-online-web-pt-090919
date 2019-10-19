e80ba72aee4f262c419f210e02b4fedd9fc0cb5e
Emily Harber <emily.a.harber@gmail.com>
2019-10-19 22:05:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -12,9 +12,9 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
-    puts "What would you like to do?"
     user_input = nil 
     until user_input != nil 
+    puts "What would you like to do?"
       user_input = gets.chomp 
     end 
   end 
